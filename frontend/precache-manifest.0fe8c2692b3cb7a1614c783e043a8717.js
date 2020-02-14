722349c8ed0a77e00bc438831f4e65600a65d9c8
sadegh <sadegh25_safikhani@yahoo.com>
2020-02-15 10:26:21 +1100

    try push app to heroku

---
+++ frontend/precache-manifest.0fe8c2692b3cb7a1614c783e043a8717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ca7091b44f875c3f07bb418742f2cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d3e6ef6082f378d02c5",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2cf0031fc12815f90d4b",
+    "url": "/static/css/main.fddb5a01.chunk.css"
+  },
+  {
+    "revision": "1d3e6ef6082f378d02c5",
+    "url": "/static/js/2.b49399e5.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.b49399e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cf0031fc12815f90d4b",
+    "url": "/static/js/main.74bd92e7.chunk.js"
+  },
+  {
+    "revision": "ebd0df65b0c9dd775322",
+    "url": "/static/js/runtime-main.266a965c.js"
+  }
+]);
